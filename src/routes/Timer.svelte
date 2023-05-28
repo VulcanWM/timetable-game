6c25c73894661103aad0abc7a713c661e5fb58b3
VulcanWM <logmedha09@gmail.com>
2023-05-28 09:50:28 +0100

    added a timer

---
+++ src/routes/Timer.svelte
@@ -0,0 +1,17 @@
+<script>
+	import { onDestroy } from 'svelte';
+
+	export let callback;
+	export let interval = 1000;
+	
+	const timer = setInterval(callback, interval);
+	onDestroy(() => {
+		clearInterval(timer);
+	});
+
+</script>
+
+<p>
+	This component executes a callback every 
+	{interval} millisecond{interval === 1 ? '' : 's'}
+</p>
