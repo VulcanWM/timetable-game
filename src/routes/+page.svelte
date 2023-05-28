ffa308a1fe08e62be240a1174644b93509277242
VulcanWM <logmedha09@gmail.com>
2023-05-28 09:56:05 +0100

    added a game ended statement

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,9 @@
 	import Timer from './Timer.svelte';
 
 	let open = false;
+	let ended = false;
 	let seconds = 60;
+	let score = 0;
 	function toggle(){
 		if (open == true){
 			open = false;
@@ -11,7 +13,13 @@
 			open = true;
 		}
 	}
-	const handleTick = () => (seconds -= 1);
+	function handleTick(){
+		seconds -= 1
+		if (seconds == 0){
+			toggle();
+			ended = true;
+		}
+	}
 </script>
 
 <div>
@@ -19,5 +27,8 @@
 	{#if open}
 		<p>{seconds}</p>
 		<Timer callback={handleTick} />
+	{/if}
+	{#if ended}
+		<p>The game has ended! Your score is {score}</p>
 	{/if}
 </div>
