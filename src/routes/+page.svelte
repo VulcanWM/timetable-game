bc8a61697543e51c6fdad742ebc78ff3f0d0a05d
VulcanWM <logmedha09@gmail.com>
2023-05-29 12:33:03 +0100

    you can answer questions

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,11 +4,13 @@
 	let open = false;
 	let ended = false;
 	let seconds = 60;
-	let score = 0;
+	let correct = 0;
+	let incorrect = 0;
 	function toggle(){
 		if (open == true){
 			open = false;
 			seconds = 60;
+			ended = false;
 		} else {
 			open = true;
 		}
@@ -23,17 +25,34 @@
 	let num1 = Math.floor(Math.random() * 20) + 1;
 	let num2 = Math.floor(Math.random() * 20) + 1;
 	$: answer = num1 * num2
+	$: percentage = Math.floor((correct/(correct+incorrect))*100)
+	let guess = ""
+	function handleSubmit() {
+		if (parseInt(guess) == answer){
+			correct += 1
+		} else {
+			incorrect += 1
+		}
+		guess = ""
+		num1 = Math.floor(Math.random() * 20) + 1;
+		num2 = Math.floor(Math.random() * 20) + 1;
+	}
 </script>
 
 <div>
-	<button on:click={toggle}>{open ? 'Stop' : 'Start'}</button>
+	<button on:click={toggle}>{open ? 'End Game' : 'Start'}</button>
 	{#if open}
+		<p>Correct: {correct}</p>
+		<p>Incorrect: {incorrect}</p>
 		<p>{seconds}</p>
 		<Timer callback={handleTick} />
 		<p>What is {num1} x {num2}?</p>
-		<input/>
+		<form on:submit|preventDefault={handleSubmit}>
+			<input bind:value={guess}/>
+		</form>
 	{/if}
 	{#if ended}
-		<p>The game has ended! Your score is {score}</p>
+		<p>The game has ended! You got {correct} questions correct and {incorrect} questions incorrect!</p>
+		<p>You got {percentage}% of questions correct!</p>
 	{/if}
 </div>
