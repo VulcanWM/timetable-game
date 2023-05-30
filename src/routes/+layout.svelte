cbf2b8106afb6645612dddc6d1a65bfc39d66702
VulcanWM <logmedha09@gmail.com>
2023-05-30 13:02:57 +0100

    added navbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,6 +3,14 @@
 </script>
 
 <body>
+    <div id="header">
+        <h1>Timetable Game</h1>
+    </div>
+    <div id="navbar">
+        <a href="/">Game</a>
+        <a href="/about">About</a>
+        <a href="https://github.com/VulcanWM/timetable-game" target="_blank">GitHub Repo</a>
+    </div>
     <div id="main">
         <slot />
     </div>
