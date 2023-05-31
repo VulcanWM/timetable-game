c199b66deb48512c85f793bb6806ecb3c4c309c2
VulcanWM <logmedha09@gmail.com>
2023-05-31 12:01:21 +0100

    added content to the about page

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -2,5 +2,9 @@
 	<title>Timetable Game - About</title>
 </svelte:head>
 <div>
-    <h1>About</h1>
+    <h1>About this game</h1>
+    <p>I (<a href="https://vulcanwm.is-a.dev" target="_blank">VulcanWM</a>) made this project with <bold>Svelte</bold> and <bold>SvelteKit</bold>.</p>
+    <p>Since I first played <a href="https://ttrockstars.com/" target="_blank">TTRockstars</a>, I've always wanted to make a game like this.</p>
+    <p>So when I started learning Svelte and needed a project idea, I decided to make this.</p>
+    <p>Check out its <a href="https://github.com/VulcanWM/timetable-game" target="_blank">GitHub Repo</a> if you're curious about how it was made.</p>
 </div>
